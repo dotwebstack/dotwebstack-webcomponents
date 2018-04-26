53d4f4662c473117ee193cd72201c6085a9c95c5
Joost Farla <joost@apiwise.nl>
2018-04-26 11:33:49 +0200

    Package as module

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
 
+const libraryName = 'dotwebstack-webcomponents';
 const env = process.env.NODE_ENV || 'development';
 const srcDir = path.resolve(__dirname, 'src');
 const buildDir = path.resolve(__dirname, 'build');
@@ -13,15 +14,38 @@ module.exports = {
   output: {
     path: buildDir,
     filename: '[name].js',
+    library: libraryName,
+    libraryTarget: 'umd',
+    umdNamedDefine: true,
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js'],
+    alias: {
+      [libraryName]: path.join(srcDir, 'index.ts'),
+    },
+  },
+  externals: {
+    react: {
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'react',
+      root: 'React',
+    },
+    'react-dom': {
+      commonjs: 'react-dom',
+      commonjs2: 'react-dom',
+      amd: 'react-dom',
+      root: 'ReactDOM',
+    },
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
         loader: 'awesome-typescript-loader',
+        query: {
+          declaration: env === 'production',
+        }
       },
     ],
   },
