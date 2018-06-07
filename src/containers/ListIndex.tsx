2b0f03f82d922e9106567a28944108a99742c7da
NathanVanDalen <nathan_vandalen@hotmail.com>
2018-06-07 13:37:30 +0200

    GRID-3169: Refactor ListIndex
    introduce variable sortFn

--- src/containers/ListIndex.tsx
+++ src/containers/ListIndex.tsx
@@ -6,23 +6,20 @@ import { lexicographicSort } from '../utils';
 export interface OwnProps {
   readonly keys: string[];
   readonly title?: string;
-}
-
-function getNavigationIndexFromSortedMapKeys(keys: string[], title?: string) {
-  return <div className="sticky-top">
-    <h2>{title}</h2>
-    <Nav className="navbar navbar-default static ">
-      <ol className="nav navbar-nav">
-        {keys.sort(lexicographicSort).map(key => (
-          <Link to={'container' + key} spy={true} smooth={true} key={key}>{key}</Link>
-        ))}
-      </ol>
-    </Nav>
-  </div>;
+  readonly sortFn?: any;
 }
 
 const ListIndex: React.StatelessComponent<OwnProps> = (props) => {
-  return getNavigationIndexFromSortedMapKeys(props.keys, props.title);
+  const { keys, title, sortFn = lexicographicSort } = props;
+
+  return <div className="sticky-top">
+    <h2>{title}</h2>
+    <Nav vertical>
+      {keys.sort(sortFn).map(key => (
+        <Link to={'container' + key} spy={true} smooth={true} key={key}>{key}</Link>
+      ))}
+    </Nav>
+  </div>;
 };
 
 export default ListIndex;
