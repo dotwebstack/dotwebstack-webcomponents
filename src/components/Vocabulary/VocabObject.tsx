ee527a886757452b8f7c46f6128d971b84f54611
joost aafjes <joostaafjes@gmail.com>
2018-06-08 17:06:49 +0200

    make items clickable

--- src/components/Vocabulary/VocabObject.tsx
+++ src/components/Vocabulary/VocabObject.tsx
@@ -32,7 +32,7 @@ const VocabObject: React.StatelessComponent<OwnProps> = (props) => {
                         {clazz.values[key].map(item => (
                           <p key={key + clazz.values[key].indexOf(item)}>
                             <Link to={'container' + item.label} smooth>
-                              {item.label}
+                              <a href={item.link}>{item.label}</a>
                             </Link>
                           </p>
                         ))}
