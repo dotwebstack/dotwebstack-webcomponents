7b03de281ec3db5145ab720c33e6e43fa35363f3
Joost Farla <joost.farla@kadaster.nl>
2018-08-06 16:45:58 +0200

    Show related classes

--- src/components/PropertyList.tsx
+++ src/components/PropertyList.tsx
@@ -3,7 +3,7 @@ import { Term } from 'rdf-js';
 import { namedNode } from 'rdf-data-model';
 import Store from '../lib/Store';
 import { compareTerm, localName } from '../utils';
-import { DCT, RDFS, SKOS } from '../namespaces';
+import { DCT, RDFS, SHACL, SKOS } from '../namespaces';
 
 type Props = {
   propertyIris: Term[],
@@ -26,11 +26,42 @@ const findDefinition = (propertyIri: Term, store: Store): Term | undefined => {
   return store.findObjects(conceptIri, namedNode(SKOS + 'definition'))[0];
 };
 
-const findSuperPropertyIris = (classIri: Term, store: Store): Term[] =>
-  store.findObjects(classIri, namedNode(RDFS + 'subPropertyOf'));
+const findSuperPropertyIris = (propertyIri: Term, store: Store): Term[] =>
+  store.findObjects(propertyIri, namedNode(RDFS + 'subPropertyOf'));
 
-const findSubPropertyIris = (classIri: Term, store: Store): Term[] =>
-  store.findSubjects(namedNode(RDFS + 'subPropertyOf'), classIri);
+const findSubPropertyIris = (propertyIri: Term, store: Store): Term[] =>
+  store.findSubjects(namedNode(RDFS + 'subPropertyOf'), propertyIri);
+
+const findUsedInClassIris = (propertyIri: Term, store: Store): Term[] =>
+  store.findSubjects(namedNode(SHACL + 'path'), propertyIri)
+    .map((propertyShapeIri) => {
+      const classShapeIri = store.findSubjects(namedNode(SHACL + 'property'), propertyShapeIri)[0];
+
+      if (classShapeIri === undefined) {
+        throw new Error(`No class shape found for property shape ${propertyShapeIri.value}`);
+      }
+
+      const classIri = store.findObjects(classShapeIri, namedNode(SHACL + 'targetClass'))[0];
+
+      if (classIri === undefined) {
+        throw new Error(`No class found for class shape ${classShapeIri.value}`);
+      }
+
+      return classIri;
+    });
+
+const findRelatedClassIri = (propertyIri: Term, store: Store): Term | undefined =>
+  store.findSubjects(namedNode(SHACL + 'path'), propertyIri)
+    .reduce(
+      (acc: Term | undefined, propertyShapeIri) => {
+        if (acc !== undefined) {
+          return acc;
+        }
+
+        return store.findObjects(propertyShapeIri, namedNode(SHACL + 'class'))[0];
+      },
+      undefined,
+    );
 
 const propertyList: React.StatelessComponent<Props> = ({ propertyIris, store }) => (
   <ol className="list-unstyled">
@@ -38,6 +69,8 @@ const propertyList: React.StatelessComponent<Props> = ({ propertyIris, store }) 
       const definition = findDefinition(propertyIri, store);
       const superPropertyIris = findSuperPropertyIris(propertyIri, store).sort(compareTerm);
       const subPropertyIris = findSubPropertyIris(propertyIri, store).sort(compareTerm);
+      const usedInClassIris = findUsedInClassIris(propertyIri, store).sort(compareTerm);
+      const relatedClassIri = findRelatedClassIri(propertyIri, store);
 
       return (
         <li key={propertyIri.value} id={localName(propertyIri)}>
@@ -73,6 +106,28 @@ const propertyList: React.StatelessComponent<Props> = ({ propertyIris, store }) 
                         </li>
                       ))}
                     </ul>
+                  </td>
+                </tr>
+              )}
+              {usedInClassIris.length > 0 && (
+                <tr>
+                  <th scope="row">Eigenschap van:</th>
+                  <td>
+                    <ul className="list-unstyled">
+                      {usedInClassIris.map(classIris => (
+                        <li key={classIris.value}>
+                          <a href={classIris.value}>{localName(classIris)}</a>
+                        </li>
+                      ))}
+                    </ul>
+                  </td>
+                </tr>
+              )}
+              {relatedClassIri && (
+                <tr>
+                  <th scope="row">Gerelateerde klasse:</th>
+                  <td>
+                    <a href={relatedClassIri.value}>{localName(relatedClassIri)}</a>
                   </td>
                 </tr>
               )}
