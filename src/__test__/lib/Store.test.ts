698aea8046e072db068aeca68b7137c876ef7946
JarvanErp <jarverp@gmail.com>
2018-10-16 15:56:27 +0200

    Refactor Store creation in Store.test

--- src/__test__/lib/Store.test.ts
+++ src/__test__/lib/Store.test.ts
@@ -2,56 +2,55 @@ import Store from '../../lib/Store';
 import { literal2, namedNodeSubject1, namedNodePredicate1, namedNodePredicate2,
   namedNodeObject1, namedNodeObject2, quadWithLiteral1, quadWithLiteral2, quadWith1,
    quadWithObject2, quadWithSubject2, quadWithPredicateObject2 } from '../TestData';
+import { Quad } from 'rdf-js';
+
+function createStore(quads: Quad[]) {
+  return new Store(quads);
+}
 
 describe('Store::findStatements', () => {
   it('returns result when statement found', () => {
-    const store = new Store([quadWithLiteral1]);
-    const result = store.findStatements(namedNodeSubject1);
+    const result = createStore([quadWithLiteral1]).findStatements(namedNodeSubject1);
     expect(result).toEqual([quadWithLiteral1]);
   });
 
   it('returns empty list when no statement found', () => {
-    const store = new Store([quadWithLiteral2]);
-    const result = store.findStatements(literal2);
+    const result = createStore([quadWithLiteral2]).findStatements(literal2);
     expect(result).toEqual([]);
   });
 });
 
 describe('Store::findSubject', () => {
   it('returns result when subject found', () => {
-    const store = new Store([quadWith1]);
-    const result = store.findSubjects(namedNodePredicate1, namedNodeObject1);
+    const result = createStore([quadWith1]).findSubjects(namedNodePredicate1, namedNodeObject1);
     expect(result).toEqual([namedNodeSubject1]);
   });
 
   it('returns empty list when no subject found', () => {
-    const store = new Store([quadWithObject2]);
-    const result = store.findSubjects(namedNodePredicate1, namedNodeObject1);
+    const result = createStore([quadWithObject2]).findSubjects(namedNodePredicate1, namedNodeObject1);
     expect(result).toEqual([]);
   });
 
   it('returns multiple results when multiple objects are given', () => {
-    const store = new Store([quadWith1, quadWithObject2]);
-    const result = store.findSubjects(namedNodePredicate1, [namedNodeObject1, namedNodeObject2]);
+    const result = createStore([quadWith1, quadWithObject2])
+    .findSubjects(namedNodePredicate1, [namedNodeObject1, namedNodeObject2]);
     expect(result).toEqual([namedNodeSubject1, namedNodeSubject1]);
   });
 });
 
 describe('Store::findObject', () => {
   it('returns result when object found', () => {
-    const store = new Store([quadWith1]);
-    const result = store.findObjects(namedNodeSubject1, namedNodePredicate1);
+    const result = createStore([quadWith1]).findObjects(namedNodeSubject1, namedNodePredicate1);
     expect(result).toEqual([namedNodeObject1]);
   });
 
   it('returns empty list when no object found', () => {
-    const store = new Store([quadWithSubject2]);
-    const result = store.findObjects(namedNodeSubject1, namedNodePredicate1);
+    const result = createStore([quadWithSubject2]).findObjects(namedNodeSubject1, namedNodePredicate1);
     expect(result).toEqual([]);
   });
   it('returns multiple results when multiple predicates are given', () => {
-    const store = new Store([quadWith1, quadWithPredicateObject2]);
-    const result = store.findObjects(namedNodeSubject1, [namedNodePredicate1, namedNodePredicate2]);
+    const result = createStore([quadWith1, quadWithPredicateObject2])
+    .findObjects(namedNodeSubject1, [namedNodePredicate1, namedNodePredicate2]);
     expect(result).toEqual([namedNodeObject1, namedNodeObject2]);
   });
 });
