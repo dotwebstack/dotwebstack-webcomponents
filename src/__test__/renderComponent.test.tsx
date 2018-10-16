0f3318c26c50df0b218b838ac78e78b15b704553
Fleur Oudenampsen <fleur.oudenampsen@kadaster.nl>
2018-10-16 14:29:07 +0200

    GRID-3717: simplify renderComponent

--- src/__test__/renderComponent.test.tsx
+++ src/__test__/renderComponent.test.tsx
@@ -13,25 +13,17 @@ describe('renderComponent', () => {
   it('renders component when found', () => {
     const ontology = namedNode('http://foo');
     const store = new Store([]);
-
-    const config = {
-      name: 'Vocabulary',
-      props: { ontology, store },
-    };
+    const props = { ontology, store };
 
     const div = document.createElement('div');
-    renderComponent(config, div);
+    renderComponent(div, 'Vocabulary', props);
     expect(render).toHaveBeenCalledTimes(1);
     expect(render).toHaveBeenCalledWith(<Vocabulary ontology={ontology} store={store} />, div);
   });
 
   it('throws error when component not found', () => {
-    const config = {
-      name: 'Foo',
-    };
-
     const div = document.createElement('div');
-    expect(() => renderComponent(config, div)).toThrowError();
+    expect(() => renderComponent(div, 'Foo')).toThrowError();
   });
 })
 ;
