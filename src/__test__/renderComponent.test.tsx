8d7d47ed92eb463f56bc8aa0126e89ef2b0dc2e6
JarvanErp <jarverp@gmail.com>
2018-10-23 11:49:50 +0200

    GRID-3717 restore graphContext

--- src/__test__/renderComponent.test.tsx
+++ src/__test__/renderComponent.test.tsx
@@ -4,8 +4,17 @@ import { render } from 'react-dom';
 import { namedNode } from '@rdfjs/data-model';
 import Store from '../lib/Store';
 import Vocabulary from '../components/Vocabulary';
+import { graphContext } from '../components/GraphContext';
+import fetchMock = require('fetch-mock');
+
+const fooJsonLd = {
+  'http://schema.org/name': [{ '@value': 'Manu Sporny' }],
+  'http://schema.org/url': [{ '@id': 'http://manu.sporny.org/' }],
+  'http://schema.org/image': [{ '@id': 'http://manu.sporny.org/images/manu.png' }],
+};
 
 jest.mock('react-dom');
+fetchMock.mock('http://example.org', fooJsonLd);
 
 afterEach(jest.resetAllMocks);
 
@@ -25,5 +34,25 @@ describe('renderComponent', () => {
     const div = document.createElement('div');
     expect(() => renderComponent(div, 'Foo')).toThrowError();
   });
-})
-;
+
+  it('renders with graphContext', async () => {
+    const endpoint = 'http://example.org';
+    const div = document.createElement('div');
+    const ontology = namedNode('http://schema.org/url');
+    let testStore = new Store([]);
+    await graphContext(endpoint)
+    .then((store) => {
+      testStore = store;
+      renderComponent(
+        div,
+        'Vocabulary',
+        {
+          store,
+          ontology,
+        },
+      );
+    });
+    expect(render).toHaveBeenCalledTimes(1);
+    expect(render).toHaveBeenCalledWith(<Vocabulary ontology={ontology} store={testStore} />, div);
+  });
+});
