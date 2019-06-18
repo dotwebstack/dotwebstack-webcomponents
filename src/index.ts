d047bfe8a310794c95086e66d007397589c772e0
Pano Maria <pano.maria@gmail.com>
2019-06-18 11:17:59 +0200

    NBIZ-285: create and render components by string reference
    * adds a simple component registry
    * adds support for string references in renderComponent and createComponent
    * makes non-react config possible

--- src/index.ts
+++ src/index.ts
@@ -18,11 +18,38 @@ import TupleSearch from './components/TupleSearch';
 import GraphSearch from './components/GraphSearch';
 import Value, { ValueProps } from './components/Value';
 
-export const createComponent = (component: React.ComponentType<any>, props?: any) =>
-  React.createElement(component, props);
+export const createComponent = (component: React.ComponentType<any> | string, props?: any) =>
+  React.createElement(resolveComponent(component), props);
 
-export const renderComponent = (container: HTMLElement, component: React.ComponentType<any>, props?: any) =>
-  ReactDOM.render(createComponent(component, props), container);
+export const renderComponent = (container: HTMLElement, component: React.ComponentType<any> | string, props?: any) =>
+  ReactDOM.render(createComponent(resolveComponent(component), props), container);
+
+const resolveComponent = (component: React.ComponentType<any> | string) : React.ComponentType<any> | string => {
+  if (typeof component === 'string') {
+    const entry = componentRegistry.get(component);
+    if (entry) {
+      return entry;
+    }
+  }
+  return component;
+};
+
+// Make components resolveable by string reference
+const componentRegistry = new Map<string, React.ComponentType<any>>();
+componentRegistry.set('Label', Label);
+componentRegistry.set('Value', Value);
+componentRegistry.set('ConceptList', ConceptList);
+componentRegistry.set('GraphContext', GraphContext);
+componentRegistry.set('GraphSearch', GraphContext);
+componentRegistry.set('Resource', Resource);
+componentRegistry.set('Vocabulary', Vocabulary);
+componentRegistry.set('ClassTree', ClassTree);
+componentRegistry.set('PropertyTree', PropertyTree);
+componentRegistry.set('PropertyList', PropertyList);
+componentRegistry.set('ClassList', ClassList);
+componentRegistry.set('TupleContext', TupleContext);
+componentRegistry.set('TupleList', TupleList);
+componentRegistry.set('TupleSearch', TupleSearch);
 
 export {
   Label,
